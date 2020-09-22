18a20b4cab2fc2cfe9426d7fc76f6f506f66c24c
tcharfi66 <charfitej6@gmail.com>
2020-09-22 14:35:00 -0400

    now working on homepage

--- dealership/config/routes.rb
+++ dealership/config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   resources :cars
   resources :dealers
   resources :customers
   resources :inventories
+
+
+
+
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
